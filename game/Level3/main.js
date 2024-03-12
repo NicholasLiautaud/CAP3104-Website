a3ecb501880698f65c09ef60a512673528066285
sorrxw <143142285+sorrxw@users.noreply.github.com>
2024-03-12 06:53:22 -0400

    Add files via upload

--- game/Level3/main.js
+++ game/Level3/main.js
@@ -1,6 +1,78 @@
 function checkQuestionOne()
 {
     var answer1 = document.getElementById("question_one_answer").value;
+    if (answer1 == "OWEN ROBERTS")
+    {
+        alert("Correct!");
+        document.getElementById("question_one_submit").disabled = true;
+    }
+    else
+    {
+        alert("Incorrect!");
+    }
+}
+
+function checkQuestionTwo()
+{
+    var answer2 = document.getElementById("question_two_answer").value;
+    if (answer2 == "GCM")
+    {
+        alert("Correct!");
+        document.getElementById("question_two_submit").disabled = true;
+    }
+    else
+    {
+        alert("Incorrect!");
+    }
+}
+
+function checkQuestionThree()
+{
+    var answer3 = document.getElementById("question_three_answer").value;
+    if (answer3 == "BURJ KHALIFA")
+    {
+        alert("Correct!");
+        document.getElementById("question_three_submit").disabled = true;
+    }
+    else
+    {
+        alert("Incorrect!");
+    }
+}
+
+function checkQuestionFour()
+{
+    var answer4 = document.getElementById("question_four_answer").value;
+    if (answer4 == "SALVADOR DALI")
+    {
+        alert("Correct!");
+        document.getElementById("question_four_submit").disabled = true;
+    }
+    else
+    {
+        alert("Incorrect!");
+    }
+}
+
+function checkQuestionFive()
+{
+    var answer5 = document.getElementById("question_five_answer").value;
+    if (answer5 == "THE PERSISTENCE OF MEMORY")
+    {
+        alert("Correct!");
+        document.getElementById("question_five_submit").disabled = true;
+    }
+    else
+    {
+        alert("Incorrect!");
+    }
+}
+
+// beginning of legacy code
+
+/* function checkQuestionOne()
+{
+    var answer1 = document.getElementById("question_one_answer").value;
     if (answer1 == "Owen Roberts" || answer1 == "owen Roberts" || answer1 == "Owen roberts" || answer1 == "owen roberts")
     {
         alert("Correct!");
@@ -25,3 +97,47 @@ function checkQuestionTwo()
         alert("Incorrect!");
     }
 }
+
+function checkQuestionThree()
+{
+    var answer3 = document.getElementById("question_three_answer").value;
+    if (answer3 == "Burj Khalifa" || answer3 == "burj khalifa" || answer3 == "burj Khalifa" || answer3 == "Burj khalifa")
+    {
+        alert("Correct!");
+        document.getElementById("question_three_submit").disabled = true;
+    }
+    else
+    {
+        alert("Incorrect!");
+    }
+}
+
+function checkQuestionFour()
+{
+    var answer4 = document.getElementById("question_four_answer").value;
+    if (answer4 == "Salvador Dali" || answer4 == "salvador dali" || answer4 == "salvador Dali" || answer4 == "Salvador dali")
+    {
+        alert("Correct!");
+        document.getElementById("question_four_submit").disabled = true;
+    }
+    else
+    {
+        alert("Incorrect!");
+    }
+}
+
+function checkQuestionFive()
+{
+    var answer5 = document.getElementById("question_five_answer").value;
+    if (answer5 == "The Persistence of Memory" || answer5 == "the persistence of memory" || answer5 == "the persistence of memory" || answer5 == "Salvador dali")
+    {
+        alert("Correct!");
+        document.getElementById("question_five_submit").disabled = true;
+    }
+    else
+    {
+        alert("Incorrect!");
+    }
+} */
+
+// ending of legacy code
